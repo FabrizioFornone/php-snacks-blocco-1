7b1d972236800e2e4595abbd8192ab4b00590907
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-02-15 19:08:38 +0100

    Exercise 2 improved

--- snack-2/index.php
+++ snack-2/index.php
@@ -8,15 +8,34 @@ che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è
 
 */
 
-$name = $_GET["name"];
+if (key_exists('name', $_GET)) {
+    $name = $_GET["name"];
+} else {
+    
+    $name = " <h1> name = empty </h1>";
+    echo $name;
+}
 
-$mail = $_GET["mail"];
-$age = $_GET["age"];
+if (key_exists('mail', $_GET)) {
+    $mail = $_GET["mail"];
+} else {
+    $mail = " <h1> mail = empty </h1>";
+    echo $mail;
+}
+
+if (key_exists('age', $_GET)) {
+    $age = $_GET["age"];
+} else {
+    $age = " <h1> age = empty </h1>";
+    echo $age;
+}
+
+
 
 
 if (strlen($name) > 3 && strpos($mail, '@',) && strpos($mail, '.') && ctype_digit($age)) {
 
-    echo " <h1> Login Successful </h1>";
+    echo " <h1> Login Successful </h1> <h2> $name </h2> <h2> $mail </h2> <h2> $age </h2>";
 } else {
 
     echo  " <h1> Login Denied </h1>";
